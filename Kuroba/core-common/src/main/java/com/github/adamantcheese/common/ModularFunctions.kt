f6f35688fbd2fbe97d34b43a57f49f2332793239
k1rakishou <kez1911@protonmail.com>
2020-07-05 11:39:14 +0300

    Add @CheckReturnValue annotation for all ModularResult methods that return ModularResult

--- Kuroba/core-common/src/main/java/com/github/adamantcheese/common/ModularFunctions.kt
+++ Kuroba/core-common/src/main/java/com/github/adamantcheese/common/ModularFunctions.kt
@@ -1,21 +1,21 @@
 package com.github.adamantcheese.common
 
 interface VoidFunction {
-    fun invoke()
+  fun invoke()
 }
 
 interface ModularFunction<T> {
-    fun invoke(param: T)
+  fun invoke(param: T)
 }
 
 interface ModularFunction2<T1, T2> {
-    fun invoke(param1: T1, param2: T2)
+  fun invoke(param1: T1, param2: T2)
 }
 
 interface GenericFunction<R> {
-    fun invoke(): R
+  fun invoke(): R
 }
 
 interface GenericModularFunction<T, R> {
-    fun invoke(param: T): R
+  fun invoke(param: T): R
 }
--- Kuroba/core-common/src/main/java/com/github/adamantcheese/common/ModularResult.kt
+++ Kuroba/core-common/src/main/java/com/github/adamantcheese/common/ModularResult.kt
@@ -1,125 +1,132 @@
 package com.github.adamantcheese.common
 
 import java.util.*
+import javax.annotation.CheckReturnValue
 
 sealed class ModularResult<V : Any?> {
-    data class Value<V : Any?>(val value: V) : ModularResult<V>()
-    data class Error<V : Any?>(val error: Throwable) : ModularResult<V>()
+  data class Value<V : Any?>(val value: V) : ModularResult<V>()
+  data class Error<V : Any?>(val error: Throwable) : ModularResult<V>()
 
-    fun peekError(func: (Throwable) -> Unit): ModularResult<V> {
-        when (this) {
-            is Value -> return this
-            is Error -> {
-                func(this.error)
-                return this
-            }
-        }
+  @CheckReturnValue
+  fun peekError(func: (Throwable) -> Unit): ModularResult<V> {
+    when (this) {
+      is Value -> return this
+      is Error -> {
+        func(this.error)
+        return this
+      }
+    }
+  }
+
+  @CheckReturnValue
+  fun peekValue(func: (V) -> Unit): ModularResult<V> {
+    when (this) {
+      is Value -> {
+        func(this.value)
+        return this
+      }
+      is Error -> return this
+    }
+  }
+
+  fun valueOrNull(): V? {
+    if (this is Value) {
+      return value
     }
 
-    fun peekValue(func: (V) -> Unit): ModularResult<V> {
-        when (this) {
-            is Value -> {
-                func(this.value)
-                return this
-            }
-            is Error -> return this
-        }
+    return null
+  }
+
+  fun errorOrNull(): Throwable? {
+    if (this is Error) {
+      return error
     }
 
-    fun valueOrNull(): V? {
-        if (this is Value) {
-            return value
-        }
+    return null
+  }
 
-        return null
+  @CheckReturnValue
+  inline fun <T : Any?> mapValue(func: (value: V) -> T): ModularResult<T> {
+    return when (this) {
+      is Error -> error(error)
+      is Value -> Try { func(value) }
+    }
+  }
+
+  @CheckReturnValue
+  @Suppress("UNCHECKED_CAST")
+  inline fun mapError(mapper: (error: Throwable) -> Throwable): ModularResult<V> {
+    return when (this) {
+      is Error -> error(mapper(error))
+      is Value -> this
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  inline fun <T : Any?> mapErrorToValue(mapper: (error: Throwable) -> T): T {
+    return when (this) {
+      is Error -> mapper(error)
+      is Value -> value as T
+    }
+  }
+
+  fun ignore() {
+    // No-op. Just an indicator that we don't care about handling this result. This is just so
+    // it's obvious that the original intention was to ignore handling the result not that it
+    // was forgotten completely.
+  }
+
+  /**
+   * This is a handy function for cases when you want to only log the error and the return from
+   * the function. In case of a value we won't log anything and will just return the value.
+   * */
+  inline fun safeUnwrap(handler: (Throwable) -> Nothing): V {
+    return when (this) {
+      is Error -> handler(error)
+      is Value -> value
+    }
+  }
+
+  fun unwrap(): V {
+    return when (this) {
+      is Error -> throw error
+      is Value -> value
+    }
+  }
+
+  override fun toString(): String {
+    return when (this) {
+      is Value -> value?.toString() ?: "ModularResult.Value{null}"
+      is Error -> String.format(
+        Locale.ENGLISH,
+        "ModularResult.Error{%s, message: %s}",
+        error.javaClass.simpleName,
+        error.message ?: "No error message"
+      )
+    }
+  }
+
+  companion object {
+    @CheckReturnValue
+    @JvmStatic
+    fun <V : Any?> value(value: V): ModularResult<V> {
+      return Value(value)
     }
 
-    fun errorOrNull(): Throwable? {
-        if (this is Error) {
-            return error
-        }
-
-        return null
+    @CheckReturnValue
+    @JvmStatic
+    fun <V : Any?> error(error: Throwable): ModularResult<V> {
+      return Error(error)
     }
 
-    inline fun <T : Any?> mapValue(func: (value: V) -> T): ModularResult<T> {
-        return when (this) {
-            is Error -> error(error)
-            is Value -> Try { func(value) }
-        }
+    @Suppress("FunctionName")
+    inline fun <T> Try(func: () -> T): ModularResult<T> {
+      return try {
+        value(func())
+      } catch (error: Throwable) {
+        error(error)
+      }
     }
 
-    @Suppress("UNCHECKED_CAST")
-    inline fun mapError(mapper: (error: Throwable) -> Throwable): ModularResult<V> {
-        return when (this) {
-            is Error -> error(mapper(error))
-            is Value -> this
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    inline fun <T : Any?> mapErrorToValue(mapper: (error: Throwable) -> T): T {
-        return when (this) {
-            is Error -> mapper(error)
-            is Value -> value as T
-        }
-    }
-
-    fun ignore() {
-        // No-op. Just an indicator that we don't care about handling this result. This is just so
-        // it's obvious that the original intention was to ignore handling the result not that it
-        // was forgotten completely.
-    }
-
-    /**
-     * This is a handy function for cases when you want to only log the error and the return from
-     * the function. In case of a value we won't log anything and will just return the value.
-     * */
-    inline fun safeUnwrap(handler: (Throwable) -> Nothing): V {
-        return when (this) {
-            is Error -> handler(error)
-            is Value -> value
-        }
-    }
-    
-    fun unwrap(): V {
-        return when (this) {
-            is Error -> throw error
-            is Value -> value
-        }
-    }
-
-    override fun toString(): String {
-        return when (this) {
-            is Value -> value?.toString() ?: "ModularResult.Value{null}"
-            is Error -> String.format(
-                    Locale.ENGLISH,
-                    "ModularResult.Error{%s, message: %s}",
-                    error.javaClass.simpleName,
-                    error.message ?: "No error message"
-            )
-        }
-    }
-
-    companion object {
-        @JvmStatic
-        fun <V : Any?> value(value: V): ModularResult<V> {
-            return Value(value)
-        }
-
-        @JvmStatic
-        fun <V : Any?> error(error: Throwable): ModularResult<V> {
-            return Error(error)
-        }
-
-        @Suppress("FunctionName")
-        inline fun <T> Try(func: () -> T): ModularResult<T> {
-            return try {
-                value(func())
-            } catch (error: Throwable) {
-                error(error)
-            }
-        }
-
-    }
+  }
 }
