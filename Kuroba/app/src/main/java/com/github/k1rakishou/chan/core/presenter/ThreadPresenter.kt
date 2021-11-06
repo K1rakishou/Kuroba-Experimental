5f180ae34fd36ad24e8df59b84fc3e629926f2d6
k1rakishou <kez1911@protonmail.com>
2021-11-06 12:28:43 +0300

    Show toast when marked post is not present in the ChanThread (deleted).

--- Kuroba/app/src/main/java/com/github/k1rakishou/chan/core/presenter/ThreadPresenter.kt
+++ Kuroba/app/src/main/java/com/github/k1rakishou/chan/core/presenter/ThreadPresenter.kt
@@ -18,6 +18,7 @@ package com.github.k1rakishou.chan.core.presenter
 
 import android.content.Context
 import android.text.TextUtils
+import android.widget.Toast
 import androidx.annotation.StringRes
 import com.github.k1rakishou.ChanSettings
 import com.github.k1rakishou.chan.R
@@ -1185,6 +1186,7 @@ class ThreadPresenter @Inject constructor(
       val markedPost = chanThreadManager.findPostByPostNo(currentChanDescriptor, markedPostNo)
       if (markedPost == null) {
         Logger.e(TAG, "handleMarkedPost() Failed to find post ($currentChanDescriptor, $markedPostNo)")
+        showToast(context, getString(R.string.failed_post_find_post_to_scroll_to, markedPostNo), Toast.LENGTH_LONG)
         return@getAndConsumeMarkedPostNo
       }
 
