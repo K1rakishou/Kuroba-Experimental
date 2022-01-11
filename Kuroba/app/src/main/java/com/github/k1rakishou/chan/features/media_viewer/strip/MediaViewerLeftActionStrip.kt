65d292b17f95413259f2ba39f71d5e34a1ec2e66
k1rakishou <kez1911@protonmail.com>
2022-01-11 15:27:44 +0300

    Fix media viewer action strip blinking for an instant when opening media viewer with controls hidden.

--- Kuroba/app/src/main/java/com/github/k1rakishou/chan/features/media_viewer/strip/MediaViewerLeftActionStrip.kt
+++ Kuroba/app/src/main/java/com/github/k1rakishou/chan/features/media_viewer/strip/MediaViewerLeftActionStrip.kt
@@ -2,11 +2,8 @@ package com.github.k1rakishou.chan.features.media_viewer.strip
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import com.github.k1rakishou.chan.R
 import com.github.k1rakishou.chan.utils.AppModuleAndroidUtils
-import com.github.k1rakishou.chan.utils.AppModuleAndroidUtils.isTablet
-import com.github.k1rakishou.chan.utils.setVisibilityFast
 
 class MediaViewerLeftActionStrip @JvmOverloads constructor(
   context: Context,
@@ -19,12 +16,6 @@ class MediaViewerLeftActionStrip @JvmOverloads constructor(
 
     inflate(context, R.layout.media_viewer_left_action_strip, this)
     super.init()
-
-    if (isTablet()) {
-      setVisibilityFast(View.VISIBLE)
-    } else {
-      setVisibilityFast(View.GONE)
-    }
   }
 
 }
