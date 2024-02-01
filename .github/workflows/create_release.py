4d4c975388df9d37bfd7dad599b0fe79b47f7198
k1rakishou <kez1911@protonmail.com>
2024-02-01 23:26:32 +0700

    Update token, add logs.

--- .github/workflows/create_release.py
+++ .github/workflows/create_release.py
@@ -3,6 +3,10 @@ import requests
 import json
 
 def create_github_release(token, repo, tag_name, release_name, body, asset_path):
+    if (len(token) == 0):
+        print("Token is empty.")
+        return
+
     url = f"https://api.github.com/repos/{repo}/releases"
 
     headers = {
@@ -26,6 +30,7 @@ def create_github_release(token, repo, tag_name, release_name, body, asset_path)
         upload_asset(upload_url, asset_path, headers)
     else:
         print("Failed to create release.")
+        print(response.status_code)
         print(response.content)
 
 def upload_asset(upload_url, asset_path, headers):
@@ -43,13 +48,17 @@ def upload_asset(upload_url, asset_path, headers):
         print("Asset uploaded successfully.")
     else:
         print("Failed to upload asset.")
+        print(response.status_code)
         print(response.content)
 
 if __name__ == "__main__":
     token = os.getenv('GITHUB_TOKEN')
     repo = 'K1rakishou/Kuroba-Experimental-beta'
+    # TODO: use actual tag name which is going to be current version + build number
     tag_name = 'v1.0.0'
+    # TODO: use actual release name based on tag + build number
     release_name = 'Test release'
+    # TODO: use actual release description based on commits diff
     body = 'Test release body'
     asset_path = 'Kuroba/app/build/outputs/apk/beta/release/KurobaEx-beta.apk'
 
